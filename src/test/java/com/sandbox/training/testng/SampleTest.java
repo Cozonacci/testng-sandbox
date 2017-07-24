29a4390186454fbf53fd8e2cb48390654df4e6b2
Dumitru Chicu <dumitru.chicu@endava.com>
2017-07-24 15:53:02 +0100

    Sandbox for testNG

---
+++ src/test/java/com/sandbox/training/testng/SampleTest.java
@@ -0,0 +1,21 @@
+package com.sandbox.training.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by dchicu on 7/23/2017.
+ */
+public class SampleTest {
+
+    @Test
+    public void shouldTriggerTestViaTestNG() {
+        assert  1 > 0;
+        System.out.println("First TestNG assertion passed");
+    }
+
+    @Test
+    public void shouldFailedTheTest() {
+        assert  1 > 23;
+        System.out.println("Second TestNG assertion failed");
+    }
+}
